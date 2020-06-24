bce6e0db4bf2b260d29f14907ad46c845cb24f03
zzpapy <gregory.pace@hotmail.fr>
2020-06-24 13:08:41 +0200

    recherches

--- modeles/Film.js
+++ modeles/Film.js
@@ -3,8 +3,70 @@ var fetch = require('node-fetch')
 const api_key = "1209c73f14597cc45c91696c34a854c7"
 
 class Film{
-    getFilm(query){
-        const url = "https://api.themoviedb.org/3/search/movie/?query="+query+"&api_key="+api_key+"&language=en-US&page=1&include_adult=false"
+    getFilm(query,page){
+        const url = "https://api.themoviedb.org/3/search/movie/?query="+query+"&api_key="+api_key+"&language=fr-FR&page="+page+"&include_adult=false"
+        return fetch(url)
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                return json
+            })
+            .catch(function(er){
+                console.log("error",er)
+            })
+        
+    }
+    async getMovie(id){
+        let film = await fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+api_key+"&language=fr-FR")
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                return json
+            })
+            .catch(function(er){
+                console.log("error",er)
+            })
+        let cast = await fetch("https://api.themoviedb.org/3/movie/"+id+"/credits?api_key="+api_key)
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                return json
+            })
+            .catch(function(er){
+                console.log("error",er)
+            }) 
+        return {cast:cast,film:film}
+    }
+    async getActor(id){
+        let acteur = await fetch("https://api.themoviedb.org/3/person/"+id+"?api_key="+api_key+"&language=en-US")
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                return json
+            })
+            .catch(function(er){
+                console.log("error",er)
+            })
+        let films = await fetch("https://api.themoviedb.org/3/person/"+id+"/movie_credits?api_key="+api_key+"&language=en-US")
+            .then(function(res){
+                return res.json()
+            })
+            .then(function(json){
+                return json
+            })
+            .catch(function(er){
+                console.log("error",er)
+            })
+
+        return {acteur:acteur,films:films}
+        
+    }
+    findActor(query){
+        const url = "https://api.themoviedb.org/3/search/person?query="+query+"&api_key="+api_key+"&language=fr-FR&page="+page+"&include_adult=false"
         return fetch(url)
             .then(function(res){
                 return res.json()
--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,79 @@ router.get('/', async function(req, res, next) {
 
 router.post('/', async function(req, res, next) {
   let query = req.body.search
-  let movie = await film.getFilm(query)
+  if(typeof req.body.page == undefined){
+    let page = "1"    
+  }
+  else{
+    page = req.body.page
+  }
+  let movie = await film.getFilm(query,page)
   
   res.render('index', { 
     
-    film: movie
+    films: movie,
+    query: query
+  })
+})
+router.get('/film', async function(req, res, next) {
+  let id = req.query.id
+  if(typeof req.body.page == undefined){
+    let page = "1"    
+  }
+  else{
+    page = req.body.page
+  }
+  let movie = await film.getMovie(id)
+  console.log(movie)
+  res.render('detailFilm', { 
+    
+    film: movie.film,
+    cast: movie.cast
+  })
+})
+router.get('/actor', async function(req, res, next) {
+  let id = req.query.id
+ 
+  let actor = await film.getActor(id)
+  console.log(actor)
+  res.render('actor', { 
+    
+    acteur: actor.acteur,
+    films:actor.films
+  })
+})
+router.post('/actor', async function(req, res, next) {
+  let id = req.body.id
+  if(typeof req.body.page == undefined){
+    let page = "1"    
+  }
+  else{
+    page = req.body.page
+  }
+  console.log(page)
+  let actor = await film.getActor(id,page)
+  console.log(actor)
+  res.render('actor', { 
+    
+    acteur: actor.acteur,
+    films:actor.films
+  })
+})
+router.post('/actor', async function(req, res, next) {
+  let query = req.body.search
+  if(typeof req.body.page == undefined){
+    let page = "1"    
+  }
+  else{
+    page = req.body.page
+  }
+  let acteurs = await film.findActor(query,page)
+  
+  res.render('listActor', { 
+    
+    acteurs: acteurs,
+    query: query
   })
 })
 
-module.exports = router;
+module.exports = router
